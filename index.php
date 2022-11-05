315f9e7b61650d44082b4192ff1777af87fe7381
lik-alive <kirshdv@gmail.com>
2022-11-05 09:12:35 +0400

    add geo location

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ exec("zcat /var/log/nginx/access.log.*.gz", $old_logs);
 # Create a full report
 $full_log = implode("\n", $old_logs) . "\n" . implode("\n", $current_log);
 file_put_contents("access.log", $full_log);
-exec("goaccess ./access.log -o ./report.html", $res);
+exec("goaccess ./access.log --geoip-database ./dbip-country-lite* -o ./report.html", $res);
 
 # Send response
 if (file_exists("./report.html")) {
